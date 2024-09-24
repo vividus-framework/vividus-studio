eb99f3b30e577eaf00ec1d883c26ec8cdd03cf20
Valery Yatsynovich <valery_yatsynovich@epam.com>
2024-09-24 11:41:20 +0300

    Rename JDK to Java in messages shown to user
    JDK is not required, JRE is enough. JDK is renamed to Java in messages show to users to make them less restrictive.

--- src/test/suite/utils.test.ts
+++ src/test/suite/utils.test.ts
@@ -12,7 +12,7 @@ suite('Utils', () => {
         sinon.restore()
     })
 
-    const jdk13: IJavaRuntime = {
+    const java13: IJavaRuntime = {
         homedir: 'home_dir',
         version: {
             java_version: '13.0.2',
@@ -20,7 +20,7 @@ suite('Utils', () => {
         }
     }
 
-    const jdk17: IJavaRuntime = {
+    const java17: IJavaRuntime = {
         homedir: 'home_dir',
         version: {
             java_version: '17.0.2',
@@ -30,29 +30,29 @@ suite('Utils', () => {
 
     const jdkUtilsModule = require('jdk-utils')
 
-    test('Should return JDK 17 installation pointed by vividus-studio.java-home property', async () => {
-        const getRuntimeStub: SinonStub = sinon.stub(jdkUtilsModule, 'getRuntime').returns(jdk17)
+    test('Should return Java 17 installation pointed by vividus-studio.java-home property', async () => {
+        const getRuntimeStub: SinonStub = sinon.stub(jdkUtilsModule, 'getRuntime').returns(java17)
 
         sinon.stub(workspace, 'getConfiguration').returns(<WorkspaceConfiguration>{
             get: (section: string) => { return 'home_dir' }
         });
 
-        assert.equal(jdk17, await findJavaExecutable())
+        assert.equal(java17, await findJavaExecutable())
         assert.equal(true, getRuntimeStub.calledWith('home_dir', { withVersion: true }))
     })
 
-    test('Should return JDK 17 system installation', async () => {
-        const getRuntimeStub: SinonStub = sinon.stub(jdkUtilsModule, 'findRuntimes').returns([jdk17])
+    test('Should return Java 17 system installation', async () => {
+        const getRuntimeStub: SinonStub = sinon.stub(jdkUtilsModule, 'findRuntimes').returns([java17])
 
         sinon.stub(workspace, 'getConfiguration').returns(<WorkspaceConfiguration>{
             get: (section: string) => { return null }
         });
 
-        assert.equal(jdk17, await findJavaExecutable())
+        assert.equal(java17, await findJavaExecutable())
         assert.equal(true, getRuntimeStub.calledWith({ withVersion: true }))
     })
 
-    test('Should fail if vividus-studio.java-home property points to not java dir', async () => {
+    test('Should fail if vividus-studio.java-home property points to not Java dir', async () => {
         const getRuntimeStub: SinonStub = sinon.stub(jdkUtilsModule, 'getRuntime').returns(undefined)
 
         sinon.stub(workspace, 'getConfiguration').returns(<WorkspaceConfiguration>{
@@ -60,33 +60,33 @@ suite('Utils', () => {
         });
 
         await assert.rejects(findJavaExecutable(),
-            { message: 'Unable to find JDK at location specified by vividus-studio.java-home user property: home_dir' })
+            { message: 'Unable to find Java at location specified by vividus-studio.java-home user property: home_dir' })
         assert.equal(true, getRuntimeStub.calledOnce)
         assert.equal(true, getRuntimeStub.calledWith('home_dir', { withVersion: true }))
     })
 
-    test('Should fail if vividus-studio.java-home property points to old jdk versions', async () => {
-        const getRuntimeStub: SinonStub = sinon.stub(jdkUtilsModule, 'getRuntime').returns(jdk13)
+    test('Should fail if vividus-studio.java-home property points to old Java versions', async () => {
+        const getRuntimeStub: SinonStub = sinon.stub(jdkUtilsModule, 'getRuntime').returns(java13)
 
         sinon.stub(workspace, 'getConfiguration').returns(<WorkspaceConfiguration>{
             get: (section: string) => { return 'home_dir' }
         });
 
         await assert.rejects(findJavaExecutable(),
-            { message: 'The vividus-studio.java-home user property points to JDK 13.0.2 installation, but JDK 17 or higher is required' })
+            { message: 'The vividus-studio.java-home user property points to Java 13.0.2 installation, but Java 17 or higher is required' })
         assert.equal(true, getRuntimeStub.calledOnce)
         assert.equal(true, getRuntimeStub.calledWith('home_dir', { withVersion: true }))
     })
 
-    test('Should fail if there is no JDK 17 installed in the system', async () => {
-        const getRuntimeStub: SinonStub = sinon.stub(jdkUtilsModule, 'findRuntimes').returns([jdk13])
+    test('Should fail if there is no Java 17 installed in the system', async () => {
+        const getRuntimeStub: SinonStub = sinon.stub(jdkUtilsModule, 'findRuntimes').returns([java13])
 
         sinon.stub(workspace, 'getConfiguration').returns(<WorkspaceConfiguration>{
             get: (section: string) => { return null }
         });
 
         await assert.rejects(findJavaExecutable(),
-            { message: 'Unable to find JDK 17 or higher installation' })
+            { message: 'Unable to find Java 17 or higher installation' })
         assert.equal(true, getRuntimeStub.calledOnce)
         assert.equal(true, getRuntimeStub.calledWith({ withVersion: true }))
     })
