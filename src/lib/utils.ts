d46baaca2a43f4f90aa7f6613e4e113e2a866bba
Valery Yatsynovich <valery_yatsynovich@epam.com>
2024-09-24 11:54:19 +0300

    Bump minimum required Java from `17` to `21`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,6 +4,7 @@ import { workspace } from "vscode";
 export async function findJavaExecutable(): Promise<IJavaRuntime> {
     const javaHomeProperty: string = 'vividus-studio.java-home';
     const userJavaHome: string = workspace.getConfiguration().get(javaHomeProperty) as string;
+    const minJavaVersion: number = 21;
 
     if (userJavaHome != null) {
         const runtime: IJavaRuntime = await getRuntime(userJavaHome, { withVersion: true }) as IJavaRuntime;
@@ -12,9 +13,9 @@ export async function findJavaExecutable(): Promise<IJavaRuntime> {
             throw new Error(`Unable to find Java at location specified by ${javaHomeProperty} user property: ${userJavaHome}`);
         }
 
-        if (runtime.version?.major as number < 17) {
+        if (runtime.version?.major as number < minJavaVersion) {
             throw new Error(`The ${javaHomeProperty} user property points to Java ${runtime.version?.java_version} installation,`
-                + ` but Java 17 or higher is required`)
+                + ` but Java ${minJavaVersion} or higher is required`)
         }
 
         return runtime;
@@ -22,11 +23,11 @@ export async function findJavaExecutable(): Promise<IJavaRuntime> {
 
     const runtime: IJavaRuntime = (await findRuntimes({ withVersion: true })).find(runtime => {
         const version: IJavaVersion = runtime.version as IJavaVersion;
-        return version.major >= 17
+        return version.major >= minJavaVersion
     }) as IJavaRuntime;
 
     if (!runtime) {
-        throw new Error('Unable to find Java 17 or higher installation');
+        throw new Error(`Unable to find Java ${minJavaVersion} or higher installation`);
     }
 
     return runtime;
